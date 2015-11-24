e67a79549233a3e486e091ddb9f34c6722f695f2
luzaguero <luz.francisca.ac@gmail.com>
2015-11-24 04:52:26 -0300

    Programa completo p3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import leastsq
 
 np.random.seed(1911)
 
@@ -36,11 +35,30 @@ def bootstrap(pendiente, posicion):
     N_mc = 100
     pendiente = np.sort(pendiente)
     posicion = np.sort(posicion)
-    limite_bajo_1 = pendiente[int(N_mc * 0.025)]
-    limite_alto_1 = pendiente[int(N_mc * 0.975)]
-    limite_bajo_2 = posicion[int(N_mc * 0.025)]
-    limite_alto_2 = posicion[int(N_mc * 0.975)]
-    print "El intervalo de confianza al 95% para la pendiente es desde {} hasta {}".format(limite_bajo_1, limite_alto_1)
-    print "El intervalo de confianza al 95% para el coef de posicion es desde {} hasta {}".format(limite_bajo_2, limite_alto_2)
+    limbajo1 = pendiente[int(N_mc * 0.025)]
+    limalto1 = pendiente[int(N_mc * 0.975)]
+    limbajo2 = posicion[int(N_mc * 0.025)]
+    limalto2 = posicion[int(N_mc * 0.975)]
+    print "El intervalo de confianza al 95% para la pendiente es desde {} hasta {}".format(limbajo1, limalto1)
+    print "El intervalo de confianza al 95% para el coef de posicion es desde {} hasta {}".format(limbajo2, limalto2)
 
 
+# Main
+c = np.polyfit(datos()[0], datos()[2], 1)
+pendiente = montecarlo(datos()[0], datos()[1], datos()[2], datos()[3])[0]
+posicion = montecarlo(datos()[0], datos()[1], datos()[2], datos()[3])[1]
+bootstrap(pendiente, posicion)
+
+# Grafiquito
+x = np.linspace(-100, 500, 600)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+
+ax1.errorbar(datos()[0], datos()[2], xerr=datos()[1], yerr=datos()[3], fmt="go", label="Datos experimentales")
+ax1.plot(x, c[1] + x*c[0], color="m", label="Ajuste lineal")
+
+ax1.set_xlabel("Flujo banda i $[10^{-6}Jy]$")
+ax1.set_ylabel("Flujo banda z $[10^{-6}Jy]$")
+
+plt.legend(loc='lower right')
+plt.show()
